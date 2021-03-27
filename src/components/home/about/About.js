189c82f64e77313991beab43058a15e5247938a0
Trevor Reynolds <reynolds8280@gmail.com>
2021-03-27 01:56:12 -0500

    rebase?

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -21,8 +21,8 @@ const About = () => {
             <img className="profile-picture-about" alt="profilepic" src={gradPic} />
             <div className="about-text">
                 <span className="about-text-main">I'm <span className="about-name">Trevor</span></span>
-                <br/><span className="about-text-subtitle">Software Engineer</span>
-                <br/><br/>I love coding
+                <br/><span className="about-text-subtitle">Software Developer</span>
+                <br/><br/>Coding is great
                 <br/>Some of my interests are: UX/UI design, Full Stack development, App development
                 <br/><br/>If you want to know more: <a onClick={onLinkClick} href="https://docs.google.com/document/d/1_S8W1z_u70k0Z5Z1k1unJ1De4AV3L8PNml2VhypvMz0/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><span className="about-cv">here is my resume.</span></a>
                 <br/><br/>Keep scrolling to see some of the projects I've worked on.
