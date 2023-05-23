b98718195569d81f7db9c5b4ad7f3c0655840ec9
trevorreynolds2020 <44044990+trevorreynolds2020@users.noreply.github.com>
2023-05-22 23:10:06 -0400

    updates

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import gradPic from "../../../assets/images/gradpic.png"
+import pic from "../../../assets/images/pic2.png"
 
 import "../../_common/common.css"
 import "./about.css"
@@ -18,12 +18,12 @@ const About = () => {
 
     return (
         <div className="home-section container-about">        
-            <img className="profile-picture-about" alt="profilepic" src={gradPic} />
+            <img className="profile-picture-about" alt="profilepic" src={pic} />
             <div className="about-text">
                 <span className="about-text-main">I'm <span className="about-name">Trevor</span></span>
                 <br/><span className="about-text-subtitle">Software Developer</span>
                 <br/><br/>Coding is great
-                <br/>Some of my interests are: UX/UI design, Full Stack development, App development
+                <br/>Some of my interests are: Web development, UX/UI design, Making music
                 <br/><br/>If you want to know more: <a onClick={onLinkClick} href="https://docs.google.com/document/d/1_S8W1z_u70k0Z5Z1k1unJ1De4AV3L8PNml2VhypvMz0/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><span className="about-cv">here is my resume.</span></a>
                 <br/><br/>Keep scrolling to see some of the projects I've worked on.
             </div>                
